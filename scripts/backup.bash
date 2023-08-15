7264792d09fd2966a65d88044955967f16497452
pjames <pjames@gmail.com>
2023-08-15 13:12:58 +0700

    Added Wordpress backup script

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -3,11 +3,19 @@
 export $(xargs <../.env)
 
 NS=wordpress
+TAR_NAME=wp-content.tar
+DB_PASSWD=${MARIADB_ROOT_PASSWORD}
 WP_POD=$(kubectl get pods -n ${NS} --no-headers -o custom-columns=":metadata.name" | grep wordpress)
 DB_POD=wordpress-production-mysql-0
-DB_PASSWD=${MARIADB_ROOT_PASSWORD}
-
 BK_CMD="mysqldump -u root --password=${DB_PASSWD} wordpress > /tmp/wordpress_backup.sql"
 
+### mysql ###
 kubectl exec -it -n ${NS} ${DB_POD} -- /bin/bash -c "${BK_CMD}"
 kubectl cp ${NS}/${DB_POD}:/tmp/wordpress_backup.sql wordpress_backup.sql
+
+### wp-content ###
+kubectl exec -it -n ${NS} ${WP_POD} -- /bin/bash -c "cp -r /bitnami/wordpress/wp-content /tmp; cd /tmp; tar -cvf ${TAR_NAME} wp-content"
+kubectl cp ${NS}/${WP_POD}:/tmp/${TAR_NAME} ${TAR_NAME}
+
+cd ${APP_PATH}
+sudo tar -cvf ${TAR_NAME} wp-content
